e3092e9083fb2b1cf72af25dc9e8537cba19ae96
Digpolo <digpolo@gmail.com>
2022-11-14 17:52:47 -0500

    fourth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import { NavBar } from './components/navBar';
 import { Banner} from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <NavBar/>
       <Banner/>
       <Skills/>
+      <Projects/> 
     </div>
   ); 
 }
