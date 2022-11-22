a23a5e5d0b8e60c9fb537f6f8987c21c630c3b1f
Digpolo <digpolo@gmail.com>
2022-11-22 15:51:32 -0500

    second commit

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,9 +1,7 @@
 import { useState, useEffect} from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.svg'; 
-import navIcon1 from '../assets/img/nav-icon1.svg'; 
-import navIcon2 from '../assets/img/nav-icon2.svg'; 
-import navIcon3 from '../assets/img/nav-icon3.svg'; 
+
 
 
 
@@ -41,18 +39,13 @@ export const NavBar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Skills</Nav.Link>
-                <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Project</Nav.Link>
+                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Inicio')}>Inicio</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('Proyectos')} >Proyectos </Nav.Link>
             </Nav>
-            <span className="navbar-tex">
-                <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt="" /></a>
-                    <a href="#"><img src={navIcon2} alt="" /></a>
-                    <a href="#"><img src={navIcon3} alt="" /></a>
-                </div>
+            <span className="navbar-text button">
+                
                 <button className="vvd" onClick={() => console.log('conect')}>
-                    <span>Contacto</span>
+                    <span>CONTACTO</span>
                 </button>
             </span>
         </Navbar.Collapse>
