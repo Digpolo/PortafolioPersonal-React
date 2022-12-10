2c6a4a932eaa2d8f24276615236c6215877c13c9
Digpolo <digpolo@gmail.com>
2022-12-10 11:34:14 -0500

    firstCommit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,46 +3,7 @@ import logoWhite from '../assets/img/logoWhite.svg';
 import "./Footer.css";
 
 
-// export const Footer = () => {
-//     return ( 
-//         <footer className="footer">
-//             <Container>
-//                 <Row className="align- items-center">
-//                     <Col xs={12} md={6} xl={7}>
-//                         <img src={logoWhite} alt="Logo" />
-//                         <p>Copyright 2022. All Rights Reserved</p>
-//                     </Col>
-//                 </Row>
-//             </Container>
-//             <Container>
-//                 <Row className="align- items-center">
-//                     <Col xs={12} md={6} xl={7}>
-//                         <h4>Contacto</h4>
-//                         <ul>
-//                             <p>Correo</p>
-//                             <p>diegopolodev@gmail.com</p> 
-//                         </ul>
-//                     </Col>
-//                </Row>
-//             </Container>
-//             <Container>
-//                 <Row className="align- items-center">
-//                     <Col xs={12} md={6} xl={7}>
-//                         <h4>Redes Sociales</h4>
-//                         <ul>
-//                             <li>LinkedIn</li>
-//                             <li>Instagram</li> 
-//                             <li>GitHub</li> 
-//                             <li>Twtweer</li> 
 
-//                         </ul>
-//                     </Col>
-                    
-//                 </Row>
-//             </Container>    
-//         </footer>
-//     )
-// }
 
 
 export const Footer = () => {
